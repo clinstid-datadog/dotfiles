7e57aa75eb0512fa65501fca9b2b7eba2406d7f5
Chris Linstid <chris.linstid@datadoghq.com>
2022-11-17 18:10:04 +0000

    Update linux init; Install btop in workspaces

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -29,3 +29,12 @@ sudo dpkg -i nvim-linux64.deb
 popd
 
 sudo unminimize
+
+mkdir -p ~/bin
+pushd /tmp
+wget https://github.com/aristocratos/btop/releases/latest/download/btop-x86_64-linux-musl.tbz
+tar xvf btop-x86_64-linux-musl.tbz
+pushd btop
+./install.sh
+rm -rf btop btop-x86_64-linux-musl.tbz
+popd
