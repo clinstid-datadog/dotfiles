bdeccf4d308da939547292528904827e73c1ba06
Chris Linstid <chris.linstid@datadoghq.com>
2023-04-06 16:49:33 -0400

    Add public ssh key for signing

--- scripts/setup-workspace.sh
+++ scripts/setup-workspace.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
+
 curl -o /tmp/install-oh-my-zsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 KEEP_ZSHRC=yes sh /tmp/install-oh-my-zsh.sh
 
@@ -43,3 +45,6 @@ popd
 
 pip3 install dotdrop
 dotdrop install -f -p workspace
+
+cp $BASEDIR/clinstid-datadog-github_id_ed25519.pub ~/.ssh
+chmod 0644 ~/.ssh/clinstid-datadog-github_id_ed25519.pub
